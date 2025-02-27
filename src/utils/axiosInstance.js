3f1467a261346e4f01223322e7b8ece3fe8427a8
heba-mohamedd <heba.elomda2001@gmail.com>
2025-02-27 17:29:08 +0200

    new updata

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://db-server-nu.vercel.app/",
   headers: {
     "Content-Type": "application/json",
     Authorization: localStorage.getItem("token"),
